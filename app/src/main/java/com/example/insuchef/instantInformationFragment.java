e0b9c85772b64742aeb7519325cd191997295368
Yigit Kemal Can <yigit.kemal.can11@gmail.com>
2023-05-17 22:50:39 +0300

    Design commited

--- app/src/main/java/com/example/insuchef/instantInformationFragment.java
+++ app/src/main/java/com/example/insuchef/instantInformationFragment.java
@@ -3,10 +3,13 @@ package com.example.insuchef;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +62,19 @@ public class instantInformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instant_information, container, false);
+        View view = inflater.inflate(R.layout.fragment_instant_information, container, false);
+        Button calculate = view.findViewById(R.id.calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new calculationFragment());
+                fragmentTransaction.commit();
+            }
+        });
+
+
+        return view;
     }
 }
--- app/src/main/java/com/example/insuchef/mealSelectionFragment.java
+++ app/src/main/java/com/example/insuchef/mealSelectionFragment.java
@@ -3,10 +3,13 @@ package com.example.insuchef;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +62,17 @@ public class mealSelectionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meal_selection, container, false);
+        View view = inflater.inflate(R.layout.fragment_meal_selection, container, false);
+        Button distribution = view.findViewById(R.id.distribution);
+        distribution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new distributionFragment());
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
     }
 }
